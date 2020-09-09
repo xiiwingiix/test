8d2366c9e11911d531b1c92a36745e125ae2a592
Dreaming Publisher Sung! <40253284+xiiwingiix@users.noreply.github.com>
2020-09-10 00:07:28 +0900

    commit

--- js/assets/guide.js
+++ js/assets/guide.js
@@ -1,13 +1,26 @@
+$(window).scroll(function(){
+	var scroll_top = $(document).scrollTop();
+
+	// header
+	if (scroll_top > 0) {
+		$( 'header' ).hide();
+	} else {
+		$( 'header' ).show();
+	}
+
+	return false;
+});
 // main_nav
-$(document).on('click','.main_nav li', function(){
-	var is_open = $('.sub_nav').is(':visible');
+$(document).on('click','.main_nav > li', function(){
+	var item = $(this).parent().attr('class');
+	var is_open = $('.sub_nav.'+item).is(':visible');
 
 	if (is_open) {
 		$('.sub_nav').hide();
 	} else {
 		$(this).find('.sub_nav').show();
 	}
-	
+
 	return false;
 });
 
@@ -15,6 +28,6 @@ $(document).on('click','.main_nav li', function(){
 $(document).on('click','.sub_nav li', function(){
 	$('.sub_nav li.on').removeClass('on');
 	$(this).addClass('on');
-	
+
 	return false;
 });
